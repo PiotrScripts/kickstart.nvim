fceb2755df976e73febc5cdbdec4c5943bff3f9d
PiotrScripts <piotr.ireland05@gmail.com>
2025-01-19 16:23:23 +0000

    Added a Discord RPC plugin

--- init.lua
+++ init.lua
@@ -149,7 +149,7 @@ vim.opt.splitbelow = true
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
+vim.opt.list = false
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
@@ -841,6 +841,14 @@ require('lazy').setup({
         },
       }
     end,
+  },
+
+  -- Discord RPC
+  {
+    'vyfor/cord.nvim',
+    build = './build || .\\build',
+    event = 'VeryLazy',
+    opts = {}, -- calls require('cord').setup()
   },
 
   -- Themes
